415fa1e8c46c54c2fb0e93b0e36906c09d6c2600
mikedel <mcdelsi@gmail.com>
2015-12-06 23:17:28 -0500

    new sql dump

--- yourboyzreviews.sql
+++ yourboyzreviews.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 07, 2015 at 03:17 AM
+-- Generation Time: Dec 07, 2015 at 05:16 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -29,7 +29,14 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `Media` (
   `id` int(10) NOT NULL,
   `title` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Media`
+--
+
+INSERT INTO `Media` (`id`, `title`) VALUES
+(1, 'Your Highness');
 
 -- --------------------------------------------------------
 
@@ -41,7 +48,14 @@ CREATE TABLE IF NOT EXISTS `Movie` (
   `id` int(10) NOT NULL,
   `media` int(10) NOT NULL,
   `release_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Movie`
+--
+
+INSERT INTO `Movie` (`id`, `media`, `release_date`) VALUES
+(1, 0, '2011-04-11 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -84,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `User` (
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `date_created` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `User`
@@ -92,10 +106,10 @@ CREATE TABLE IF NOT EXISTS `User` (
 
 INSERT INTO `User` (`id`, `first_name`, `last_name`, `email`, `password`, `date_created`) VALUES
 (1, 'eric', 'rodey', 'test@gmail.com', 'test123', '0000-00-00 00:00:00'),
-(2, 'eric', 'rodey', 'test@gmail.com', 'test123', '0000-00-00 00:00:00'),
 (3, 'test', 'login', 'welcome@gmail.com', 'test123', '0000-00-00 00:00:00'),
 (4, 'Jack', 'Gadsten', 'jack@gmail.com', 'test123', '0000-00-00 00:00:00'),
-(5, 'p', 'sure', 'psure@t.com', 'test123', '0000-00-00 00:00:00');
+(5, 'p', 'sure', 'psure@t.com', 'test123', '0000-00-00 00:00:00'),
+(6, 'test', 'butt', 'test@butt.com', 'test1234', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -126,7 +140,15 @@ CREATE TABLE IF NOT EXISTS `UserReview` (
   `media` int(10) NOT NULL COMMENT 'foreignkey to media',
   `user` int(10) NOT NULL COMMENT 'foreignkey to user',
   `date_created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `UserReview`
+--
+
+INSERT INTO `UserReview` (`id`, `rating`, `comment`, `media`, `user`, `date_created`) VALUES
+(1, 5, 'The best commedy of all time.', 1, 6, '2015-12-06 00:00:00'),
+(2, 4, 'okie dokie', 1, 1, '2015-12-06 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -163,7 +185,8 @@ ALTER TABLE `TVShow`
 -- Indexes for table `User`
 --
 ALTER TABLE `User`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `UserRecommendation`
@@ -191,12 +214,12 @@ ALTER TABLE `UserReview`
 -- AUTO_INCREMENT for table `Media`
 --
 ALTER TABLE `Media`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `Movie`
 --
 ALTER TABLE `Movie`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `TVEpisode`
 --
@@ -211,7 +234,7 @@ ALTER TABLE `TVShow`
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `UserRecommendation`
 --
@@ -221,7 +244,7 @@ ALTER TABLE `UserRecommendation`
 -- AUTO_INCREMENT for table `UserReview`
 --
 ALTER TABLE `UserReview`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
