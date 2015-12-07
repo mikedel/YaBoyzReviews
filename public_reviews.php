5d9a379d35ee528f18464fb1584b33d8ec59f91d
rodeyfeld <ericr6493@gmail.com>
2015-12-06 23:52:00 -0500

    navbar and fixed dismiss buttons

--- public_reviews.php
+++ public_reviews.php
@@ -8,28 +8,7 @@
 
 <body>
     <section id="head-bar">
-            <nav class="navbar navbar-inverse">
-                <div class="container">
-                    <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle"collapse" data-target"#navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                    <a class="navbar-brand" href="home.php">Ya Boyz Reviews</a>
-                    <div class="collapse navbar-collapse" id="navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                            <li><a href="friend_reviews.php">Read Friend Reviews</a></li>
-                            <li><a href="recommend.php">Recommend To Friends</a></li>
-                            <li><a href="public_reviews.php">Read Public Reviews</a></li>
-                        </ul>
-                        <button type="button" class="btn btn-default navbar-btn navbar-right"><a href="signin.php">Sign In</a></button>
-                        <button type="button" class="btn btn-default navbar-btn navbar-right"><a href="signup.php">Sign Up</a></button>
-                    </div>
-                </div>
-            </nav>
+        <?php include_once 'navbar.php'; ?> 
     </section>
 
     <div class="container">
@@ -70,13 +49,14 @@
                         <?php echo $review->comment; ?>
                         </p>
                     </div>
-                </div>
                 <div class="row">
                     <div class="col-md-8"></div>
                     <div class="col-md-4">
                         <a class="btn btn-danger btn-sm">Dismiss</a>
                     </div>
                 </div>
+                </div>
+
                 <?php
             }
         ?>
